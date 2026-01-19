7335ec18e7a3db2a71e40857a80fdf7223200fac
shrfrn <shrfrn@gmail.com>
2026-01-19 10:33:19 +0200

    Done

--- js/services/yesno.service.js
+++ js/services/yesno.service.js
@@ -1,2 +1,15 @@
 'use strict'
 
+function ask(cb) {
+	const xhr = new XMLHttpRequest()
+
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+			const ans = JSON.parse(xhr.responseText)
+            cb(ans)
+		}
+	}
+    
+	xhr.open('GET', 'https://yesno.wtf/api', true)
+	xhr.send()
+}
--- js/yesno.controller.js
+++ js/yesno.controller.js
@@ -1,5 +1,27 @@
 'use strict'
 
 function onAsk() {
-    console.log('Ask...')
+    const elAnswer = document.querySelector('.answer')
+    elAnswer.classList.add('hidden')
+    
+    const elLoader = document.querySelector('.loader')
+    elLoader.classList.remove('hidden')
+
+    ask(renderAns)
+}
+
+function renderAns(res) {
+    const elAns = document.querySelector('.answer h2')
+    const elImg = document.querySelector('.answer img')
+
+    elAns.innerText = res.answer
+    elImg.src = res.image
+}
+
+function showAns() {
+    const elLoader = document.querySelector('.loader')
+    elLoader.classList.add('hidden')
+    
+    const elAnswer = document.querySelector('.answer')
+    elAnswer.classList.remove('hidden')
 }
